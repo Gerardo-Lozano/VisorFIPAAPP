683f799f7a3d984b7ad9a0ea4520b1fc82fb925e
Gerardo Lozano <65347154+Gerardo-Lozano@users.noreply.github.com>
2021-11-29 12:52:32 -0600

    Agregar archivos de proyecto.

---
+++ VisorFIPAAPP/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label46 = New System.Windows.Forms.Label()
+        Me.Label45 = New System.Windows.Forms.Label()
+        Me.Label44 = New System.Windows.Forms.Label()
+        Me.DateTimePicker4 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker3 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.DataGridView1.GridColor = System.Drawing.Color.LightSalmon
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 169)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(1578, 553)
+        Me.DataGridView1.TabIndex = 53
+        '
+        'Label46
+        '
+        Me.Label46.AutoSize = True
+        Me.Label46.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label46.Location = New System.Drawing.Point(831, 90)
+        Me.Label46.Name = "Label46"
+        Me.Label46.Size = New System.Drawing.Size(24, 20)
+        Me.Label46.TabIndex = 66
+        Me.Label46.Text = "A:"
+        '
+        'Label45
+        '
+        Me.Label45.AutoSize = True
+        Me.Label45.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label45.Location = New System.Drawing.Point(675, 90)
+        Me.Label45.Name = "Label45"
+        Me.Label45.Size = New System.Drawing.Size(34, 20)
+        Me.Label45.TabIndex = 65
+        Me.Label45.Text = "De:"
+        '
+        'Label44
+        '
+        Me.Label44.AutoSize = True
+        Me.Label44.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label44.Location = New System.Drawing.Point(747, 64)
+        Me.Label44.Name = "Label44"
+        Me.Label44.Size = New System.Drawing.Size(148, 20)
+        Me.Label44.TabIndex = 64
+        Me.Label44.Text = "Intervalo de fechas:"
+        '
+        'DateTimePicker4
+        '
+        Me.DateTimePicker4.Checked = False
+        Me.DateTimePicker4.CustomFormat = "MMM/dd/yyyy"
+        Me.DateTimePicker4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker4.Location = New System.Drawing.Point(858, 87)
+        Me.DateTimePicker4.Name = "DateTimePicker4"
+        Me.DateTimePicker4.Size = New System.Drawing.Size(111, 26)
+        Me.DateTimePicker4.TabIndex = 63
+        '
+        'DateTimePicker3
+        '
+        Me.DateTimePicker3.Checked = False
+        Me.DateTimePicker3.CustomFormat = "MMM/dd/yyyy"
+        Me.DateTimePicker3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker3.Location = New System.Drawing.Point(713, 87)
+        Me.DateTimePicker3.Name = "DateTimePicker3"
+        Me.DateTimePicker3.Size = New System.Drawing.Size(111, 26)
+        Me.DateTimePicker3.TabIndex = 62
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(727, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(188, 29)
+        Me.Label1.TabIndex = 61
+        Me.Label1.Text = "Registros Filling"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(679, 126)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(293, 28)
+        Me.Button1.TabIndex = 67
+        Me.Button1.Text = "Ejecutar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(679, 728)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(293, 28)
+        Me.Button2.TabIndex = 68
+        Me.Button2.Text = "Exportar a excel"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1602, 767)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label46)
+        Me.Controls.Add(Me.Label45)
+        Me.Controls.Add(Me.Label44)
+        Me.Controls.Add(Me.DateTimePicker4)
+        Me.Controls.Add(Me.DateTimePicker3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Visor FIPAAPP"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label46 As Label
+    Friend WithEvents Label45 As Label
+    Friend WithEvents Label44 As Label
+    Friend WithEvents DateTimePicker4 As DateTimePicker
+    Friend WithEvents DateTimePicker3 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ VisorFIPAAPP/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        connection_sql()
+
+        Try
+            'Variables para el rango de fechas
+            Dim Fecha_1 As String
+            Dim Fecha_2 As String
+
+            Fecha_1 = DateTimePicker3.Text
+            Fecha_2 = DateTimePicker4.Text
+
+            'Leer tabla de proveedores
+
+            Dim consulta As String
+            Dim lista As String
+
+            'Control de consulta dependiendo el reporte seleccionado como mixto, entradas o salidas
+
+            'consulta = "SELECT * FROM Registros_Filling"
+            consulta = "SELECT * FROM Registros_Filling WHERE hora between '" & Fecha_1 & "' and '" & Fecha_2 & "' ORDER BY WO ASC"
+            'MsgBox("el script es:  " & consulta)
+            adapt = New Odbc.OdbcDataAdapter(consulta, ConexionSQL.con)
+            ConexionSQL.reg = New DataSet
+            adapt.Fill(reg, "Tabla1")
+            lista = reg.Tables("Tabla1").Rows.Count
+            DataGridView1.DataSource = reg.Tables("Tabla1")
+        Catch ex As Exception
+            MsgBox("No se puede leer información" & ex.Message)
+
+        End Try
+
+        con.Close()
+
+        Button2.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        llenarExcel(DataGridView1)
+    End Sub
+End Class
