bfb8bacb1db205b3709c4716385c946c806a67a7
Gerardo Lozano <65347154+Gerardo-Lozano@users.noreply.github.com>
2021-12-09 11:35:21 -0600

    Se generar cambios de fecha y diseño

--- VisorFIPAAPP/Form1.Designer.vb
+++ VisorFIPAAPP/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Label46 = New System.Windows.Forms.Label()
         Me.Label45 = New System.Windows.Forms.Label()
@@ -31,7 +32,10 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
@@ -41,20 +45,22 @@ Partial Class Form1
         Me.DataGridView1.AllowUserToResizeColumns = False
         Me.DataGridView1.AllowUserToResizeRows = False
         Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.DataGridView1.GridColor = System.Drawing.Color.LightSalmon
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 169)
+        Me.DataGridView1.GridColor = System.Drawing.Color.SeaGreen
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 186)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(1578, 553)
+        Me.DataGridView1.Size = New System.Drawing.Size(984, 375)
         Me.DataGridView1.TabIndex = 53
         '
         'Label46
         '
         Me.Label46.AutoSize = True
         Me.Label46.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label46.Location = New System.Drawing.Point(831, 90)
+        Me.Label46.Location = New System.Drawing.Point(519, 85)
         Me.Label46.Name = "Label46"
         Me.Label46.Size = New System.Drawing.Size(24, 20)
         Me.Label46.TabIndex = 66
@@ -64,7 +70,7 @@ Partial Class Form1
         '
         Me.Label45.AutoSize = True
         Me.Label45.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label45.Location = New System.Drawing.Point(675, 90)
+        Me.Label45.Location = New System.Drawing.Point(363, 85)
         Me.Label45.Name = "Label45"
         Me.Label45.Size = New System.Drawing.Size(34, 20)
         Me.Label45.TabIndex = 65
@@ -74,7 +80,7 @@ Partial Class Form1
         '
         Me.Label44.AutoSize = True
         Me.Label44.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label44.Location = New System.Drawing.Point(747, 64)
+        Me.Label44.Location = New System.Drawing.Point(435, 59)
         Me.Label44.Name = "Label44"
         Me.Label44.Size = New System.Drawing.Size(148, 20)
         Me.Label44.TabIndex = 64
@@ -83,10 +89,10 @@ Partial Class Form1
         'DateTimePicker4
         '
         Me.DateTimePicker4.Checked = False
-        Me.DateTimePicker4.CustomFormat = "MMM/dd/yyyy"
+        Me.DateTimePicker4.CustomFormat = "MM/dd/yyyy"
         Me.DateTimePicker4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker4.Location = New System.Drawing.Point(858, 87)
+        Me.DateTimePicker4.Location = New System.Drawing.Point(546, 82)
         Me.DateTimePicker4.Name = "DateTimePicker4"
         Me.DateTimePicker4.Size = New System.Drawing.Size(111, 26)
         Me.DateTimePicker4.TabIndex = 63
@@ -94,10 +100,10 @@ Partial Class Form1
         'DateTimePicker3
         '
         Me.DateTimePicker3.Checked = False
-        Me.DateTimePicker3.CustomFormat = "MMM/dd/yyyy"
+        Me.DateTimePicker3.CustomFormat = "MM/dd/yyyy"
         Me.DateTimePicker3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker3.Location = New System.Drawing.Point(713, 87)
+        Me.DateTimePicker3.Location = New System.Drawing.Point(401, 82)
         Me.DateTimePicker3.Name = "DateTimePicker3"
         Me.DateTimePicker3.Size = New System.Drawing.Size(111, 26)
         Me.DateTimePicker3.TabIndex = 62
@@ -106,7 +112,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(727, 20)
+        Me.Label1.Location = New System.Drawing.Point(415, 15)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(188, 29)
         Me.Label1.TabIndex = 61
@@ -114,28 +120,59 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(679, 126)
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(367, 121)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(293, 28)
         Me.Button1.TabIndex = 67
-        Me.Button1.Text = "Ejecutar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "Ejecutar por fecha"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(679, 728)
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(367, 567)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(293, 28)
         Me.Button2.TabIndex = 68
         Me.Button2.Text = "Exportar a excel"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseVisualStyleBackColor = False
         Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(367, 155)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(293, 28)
+        Me.Button3.TabIndex = 69
+        Me.Button3.Text = "Cargar todos los registros"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(933, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(63, 64)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 70
+        Me.PictureBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1602, 767)
+        Me.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.ClientSize = New System.Drawing.Size(1008, 611)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label46)
@@ -145,11 +182,13 @@ Partial Class Form1
         Me.Controls.Add(Me.DateTimePicker3)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.DataGridView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Visor FIPAAPP"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -164,4 +203,6 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- VisorFIPAAPP/Form1.vb
+++ VisorFIPAAPP/Form1.vb
@@ -38,4 +38,34 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         llenarExcel(DataGridView1)
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        connection_sql()
+
+        Try
+
+            'Leer tabla de proveedores
+
+            Dim consulta As String
+            Dim lista As String
+
+            'Control de consulta dependiendo el reporte seleccionado como mixto, entradas o salidas
+
+            'consulta = "SELECT * FROM Registros_Filling"
+            consulta = "SELECT * FROM Registros_Filling ORDER BY WO ASC"
+            'MsgBox("el script es:  " & consulta)
+            adapt = New Odbc.OdbcDataAdapter(consulta, ConexionSQL.con)
+            ConexionSQL.reg = New DataSet
+            adapt.Fill(reg, "Tabla1")
+            lista = reg.Tables("Tabla1").Rows.Count
+            DataGridView1.DataSource = reg.Tables("Tabla1")
+        Catch ex As Exception
+            MsgBox("No se puede leer información" & ex.Message)
+
+        End Try
+
+        con.Close()
+
+        Button2.Visible = True
+    End Sub
 End Class
